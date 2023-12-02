8ad95d613bfae1098579da6d6dc2fd0ac9b228f9
spring8904 <lamthang890123music@gmail.com>
2023-12-02 12:18:56 +0700

    update

--- js/main.js
+++ js/main.js
@@ -1,26 +1,26 @@
-const studentsAPI = "https://6499635779fbe9bcf83f2553.mockapi.io/api/v1/students";
-const $ = document.querySelector.bind(document);
-const $$ = document.querySelectorAll.bind(document);
+const studentsAPI = 'https://652414aaea560a22a4e96160.mockapi.io/students'
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
 
 function start() {
-    getStudents(renderStudents);
-    handlePostForm();
+  getStudents(renderStudents)
+  handlePostForm()
 }
 
-start();
+start()
 
 //--- Functions
 function getStudents(callback) {
-    fetch(studentsAPI)
-        .then((response) => response.json())
-        .then(callback);
+  fetch(studentsAPI)
+    .then((response) => response.json())
+    .then(callback)
 }
 
 function renderStudents(students) {
-    const studentTableBody = $("#student-table tbody");
-    let count = 1;
-    const htmls = students.map(
-        (student) => `<tr>
+  const studentTableBody = $('#student-table tbody')
+  let count = 1
+  const htmls = students.map(
+    (student) => `<tr>
         <td>${count++}</td>
         <td>${student.id}</td>
         <td>${student.name}</td>
@@ -29,213 +29,228 @@ function renderStudents(students) {
         <td>${student.address}</td>
         <td>${student.phoneNumber}</td>
         <td>${student.email}</td>
-        <td><button class="btn btn--yellow" onclick="handlePutForm(${student.id})">Sửa</button></td>
-        <td><button class="btn btn--red" onclick="handleDeleteStudent(${student.id})">Xóa</button></td>
-    </tr>`
-    );
-    studentTableBody.innerHTML = htmls.join("");
+        <td><button class="btn btn--yellow" onclick="handlePutForm(${
+          student.id
+        })">Sửa</button></td>
+        <td><button class="btn btn--red" onclick="handleDeleteStudent(${
+          student.id
+        })">Xóa</button></td>
+    </tr>`,
+  )
+  studentTableBody.innerHTML = htmls.join('')
 }
 
 function handlePostStudent(data) {
-    fetch(studentsAPI, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
+  fetch(studentsAPI, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then(() => {
+      getStudents(renderStudents)
+      showAlert('Thêm thành công')
     })
-        .then((response) => response.json())
-        .then(() => {
-            getStudents(renderStudents);
-            showAlert("Thêm thành công");
-        });
 }
 
 function handleDeleteStudent(id) {
-    showConfirm("Xác nhận xóa thông tin", (result) => {
-        if (result) {
-            fetch(studentsAPI + "/" + id, {
-                method: "DELETE",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            })
-                .then((response) => response.json())
-                .then(() => {
-                    getStudents(renderStudents);
-                    showAlert("Xóa thành công");
-                });
-        }
-    });
+  showConfirm('Xác nhận xóa thông tin', (result) => {
+    if (result) {
+      fetch(studentsAPI + '/' + id, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((response) => response.json())
+        .then(() => {
+          getStudents(renderStudents)
+          showAlert('Xóa thành công')
+        })
+    }
+  })
 }
 
 function handlePutStudent(id, data) {
-    fetch(studentsAPI + "/" + id, {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
+  fetch(studentsAPI + '/' + id, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then(() => {
+      getStudents(renderStudents)
+      showAlert('Sửa thành công')
     })
-        .then((response) => response.json())
-        .then(() => {
-            getStudents(renderStudents);
-            showAlert("Sửa thành công");
-        });
 }
 
 function handlePostForm() {
-    validator({
-        form: ".post-form__main",
-        formGroupSelector: ".form-group",
-        errorSelector: ".form-message",
-        rules: [
-            validator.isRequired("#name", "Vui lòng nhập tên đầy đủ của bạn"),
-            validator.isRequired("#date-of-birth", "Vui lòng nhập ngày sinh của bạn"),
-            validator.isSelected("input[name='gender']", "Vui lòng chọn giới tính phù hợp"),
-            validator.isEmail("#email"),
-            validator.isPhoneNumber("#phone-number"),
-        ],
-        onSubmit(data) {
-            handlePostStudent(data);
-        },
-    });
+  validator({
+    form: '.post-form__main',
+    formGroupSelector: '.form-group',
+    errorSelector: '.form-message',
+    rules: [
+      validator.isRequired('#name', 'Vui lòng nhập tên đầy đủ của bạn'),
+      validator.isRequired('#date-of-birth', 'Vui lòng nhập ngày sinh của bạn'),
+      validator.isSelected(
+        "input[name='gender']",
+        'Vui lòng chọn giới tính phù hợp',
+      ),
+      validator.isEmail('#email'),
+      validator.isPhoneNumber('#phone-number'),
+    ],
+    onSubmit(data) {
+      handlePostStudent(data)
+    },
+  })
 }
 
 function handlePutForm(id) {
-    const overlay = $(".overlay");
-    const putForm = $("#put-form");
+  const overlay = $('.overlay')
+  const putForm = $('#put-form')
 
-    overlay.classList.add("active");
-    putForm.classList.add("active");
+  overlay.classList.add('active')
+  putForm.classList.add('active')
 
-    overlay.onclick = () => {
-        overlay.classList.remove("active");
-        putForm.classList.remove("active");
-    };
+  overlay.onclick = () => {
+    overlay.classList.remove('active')
+    putForm.classList.remove('active')
+  }
 
-    fillPutForm(id);
+  fillPutForm(id)
 
-    validator({
-        form: ".put-form__main",
-        formGroupSelector: ".form-group",
-        errorSelector: ".form-message",
-        rules: [
-            validator.isRequired("#name", "Vui lòng nhập tên đầy đủ của bạn"),
-            validator.isRequired("#date-of-birth", "Vui lòng nhập ngày sinh của bạn"),
-            validator.isSelected("input[name='gender']", "Vui lòng chọn giới tính phù hợp"),
-            validator.isEmail("#email"),
-            validator.isPhoneNumber("#phone-number"),
-        ],
-        onSubmit(data) {
-            handlePutStudent(id, data);
-            putForm.classList.remove("active");
-        },
-    });
+  validator({
+    form: '.put-form__main',
+    formGroupSelector: '.form-group',
+    errorSelector: '.form-message',
+    rules: [
+      validator.isRequired('#name', 'Vui lòng nhập tên đầy đủ của bạn'),
+      validator.isRequired('#date-of-birth', 'Vui lòng nhập ngày sinh của bạn'),
+      validator.isSelected(
+        "input[name='gender']",
+        'Vui lòng chọn giới tính phù hợp',
+      ),
+      validator.isEmail('#email'),
+      validator.isPhoneNumber('#phone-number'),
+    ],
+    onSubmit(data) {
+      handlePutStudent(id, data)
+      putForm.classList.remove('active')
+    },
+  })
 }
 
 function formatDate(date) {
-    const selectedDate = new Date(date);
+  const selectedDate = new Date(date)
 
-    const day = selectedDate.getDate().toString().padStart(2, "0");
-    const month = (selectedDate.getMonth() + 1).toString().padStart(2, "0");
-    const year = selectedDate.getFullYear().toString().padStart(4, "0");
+  const day = selectedDate.getDate().toString().padStart(2, '0')
+  const month = (selectedDate.getMonth() + 1).toString().padStart(2, '0')
+  const year = selectedDate.getFullYear().toString().padStart(4, '0')
 
-    return `${day}/${month}/${year}`;
+  return `${day}/${month}/${year}`
 }
 
 function formatDateForInput(date) {
-    const parts = date.split("/");
+  const parts = date.split('/')
 
-    const day = parts[0];
-    const month = parts[1];
-    const year = parts[2];
+  const day = parts[0]
+  const month = parts[1]
+  const year = parts[2]
 
-    const formattedDate = `${year}-${month}-${day}`;
-    return formattedDate;
+  const formattedDate = `${year}-${month}-${day}`
+  return formattedDate
 }
 
 function showAlert(text) {
-    const overlay = $(".overlay");
-    const alert = $(".alert");
-    const alertContent = $(".alert .content");
-    const alertBtn = $(".alert-btn .btn");
+  const overlay = $('.overlay')
+  const alert = $('.alert')
+  const alertContent = $('.alert .content')
+  const alertBtn = $('.alert-btn .btn')
 
-    overlay.classList.add("active");
-    alert.classList.add("active");
-    alertContent.innerText = text;
+  overlay.classList.add('active')
+  alert.classList.add('active')
+  alertContent.innerText = text
 
-    overlay.onclick = () => {
-        overlay.classList.remove("active");
-        alert.classList.remove("active");
-    };
+  overlay.onclick = () => {
+    overlay.classList.remove('active')
+    alert.classList.remove('active')
+  }
 
-    alertBtn.onclick = () => {
-        overlay.classList.remove("active");
-        alert.classList.remove("active");
-    };
+  alertBtn.onclick = () => {
+    overlay.classList.remove('active')
+    alert.classList.remove('active')
+  }
 }
 
 function showConfirm(text, callback) {
-    const overlay = $(".overlay");
-    const confirm = $(".confirm");
-    const confirmContent = $(".confirm .content");
-    const confirmBtn = $(".confirm-btn .btn--blue");
-    const cancelBtn = $(".confirm-btn .btn--white");
+  const overlay = $('.overlay')
+  const confirm = $('.confirm')
+  const confirmContent = $('.confirm .content')
+  const confirmBtn = $('.confirm-btn .btn--blue')
+  const cancelBtn = $('.confirm-btn .btn--white')
 
-    overlay.classList.add("active");
-    confirm.classList.add("active");
-    confirmContent.innerText = text;
+  overlay.classList.add('active')
+  confirm.classList.add('active')
+  confirmContent.innerText = text
 
-    overlay.onclick = () => {
-        overlay.classList.remove("active");
-        confirm.classList.remove("active");
-        callback(false);
-    };
+  overlay.onclick = () => {
+    overlay.classList.remove('active')
+    confirm.classList.remove('active')
+    callback(false)
+  }
 
-    cancelBtn.onclick = () => {
-        overlay.classList.remove("active");
-        confirm.classList.remove("active");
-        callback(false);
-    };
+  cancelBtn.onclick = () => {
+    overlay.classList.remove('active')
+    confirm.classList.remove('active')
+    callback(false)
+  }
 
-    confirmBtn.onclick = () => {
-        confirm.classList.remove("active");
-        callback(true);
-    };
+  confirmBtn.onclick = () => {
+    confirm.classList.remove('active')
+    callback(true)
+  }
 }
 
 function fillPutForm(id) {
-    // Lấy dữ liệu sinh viên từ bảng
-    const student = getStudentById(id);
-    // Gán giá trị vào các trường nhập liệu trong form
-    $('#put-form input[name="name"]').value = student.name;
-    $('#put-form input[value="' + student.gender + '"]').checked = true;
-    $('#put-form input[name="dateOfBirth"]').value = formatDateForInput(student.dateOfBirth);
-    $('#put-form input[name="address"]').value = student.address;
-    $('#put-form input[name="phoneNumber"]').value = student.phoneNumber;
-    $('#put-form input[name="email"]').value = student.email;
+  // Lấy dữ liệu sinh viên từ bảng
+  const student = getStudentById(id)
+  // Gán giá trị vào các trường nhập liệu trong form
+  $('#put-form input[name="name"]').value = student.name
+  $('#put-form input[value="' + student.gender + '"]').checked = true
+  $('#put-form input[name="dateOfBirth"]').value = formatDateForInput(
+    student.dateOfBirth,
+  )
+  $('#put-form input[name="address"]').value = student.address
+  $('#put-form input[name="phoneNumber"]').value = student.phoneNumber
+  $('#put-form input[name="email"]').value = student.email
 }
 
 function getStudentById(id) {
-    // Lấy danh sách sinh viên từ bảng
-    const studentRows = $$("#student-table tbody tr");
+  // Lấy danh sách sinh viên từ bảng
+  const studentRows = $$('#student-table tbody tr')
 
-    // Tìm sinh viên theo id
-    for (let i = 0; i < studentRows.length; i++) {
-        const studentId = studentRows[i].querySelector("td:nth-child(2)").textContent;
-        if (studentId === id.toString()) {
-            // Trả về dữ liệu sinh viên
-            return {
-                name: studentRows[i].querySelector("td:nth-child(3)").textContent,
-                gender: studentRows[i].querySelector("td:nth-child(4)").textContent,
-                dateOfBirth: studentRows[i].querySelector("td:nth-child(5)").textContent,
-                address: studentRows[i].querySelector("td:nth-child(6)").textContent,
-                phoneNumber: studentRows[i].querySelector("td:nth-child(7)").textContent,
-                email: studentRows[i].querySelector("td:nth-child(8)").textContent,
-            };
-        }
+  // Tìm sinh viên theo id
+  for (let i = 0; i < studentRows.length; i++) {
+    const studentId =
+      studentRows[i].querySelector('td:nth-child(2)').textContent
+    if (studentId === id.toString()) {
+      // Trả về dữ liệu sinh viên
+      return {
+        name: studentRows[i].querySelector('td:nth-child(3)').textContent,
+        gender: studentRows[i].querySelector('td:nth-child(4)').textContent,
+        dateOfBirth:
+          studentRows[i].querySelector('td:nth-child(5)').textContent,
+        address: studentRows[i].querySelector('td:nth-child(6)').textContent,
+        phoneNumber:
+          studentRows[i].querySelector('td:nth-child(7)').textContent,
+        email: studentRows[i].querySelector('td:nth-child(8)').textContent,
+      }
     }
+  }
 
-    return null;
+  return null
 }
--- js/validator.js
+++ js/validator.js
@@ -1,221 +1,242 @@
 function validator(options) {
-    const formEl = document.querySelector(options.form);
-    const selectorRules = {};
+  const formEl = document.querySelector(options.form)
+  const selectorRules = {}
 
-    if (formEl) {
-        formEl.onsubmit = (e) => {
-            e.preventDefault();
+  if (formEl) {
+    formEl.onsubmit = (e) => {
+      e.preventDefault()
 
-            let isFormValid = true;
-            options.rules.forEach((rule) => {
-                const inputEl = formEl.querySelector(rule.selector);
-                if (inputEl) {
-                    const isValid = !validate(rule, inputEl);
-                    if (!isValid) {
-                        isFormValid = false;
-                    }
-                }
-            });
+      let isFormValid = true
+      options.rules.forEach((rule) => {
+        const inputEl = formEl.querySelector(rule.selector)
+        if (inputEl) {
+          const isValid = !validate(rule, inputEl)
+          if (!isValid) {
+            isFormValid = false
+          }
+        }
+      })
 
-            // Lấy value từ form
-            if (isFormValid) {
-                if (typeof options.onSubmit === "function") {
-                    const enableInputs = formEl.querySelectorAll("[name]");
-                    const formValues = Array.from(enableInputs).reduce((values, input) => {
-                        switch (input.type) {
-                            case "radio":
-                                values[input.name] = formEl.querySelector(
-                                    'input[name="' + input.name + '"]:checked'
-                                ).value;
-                                break;
-                            case "checkbox":
-                                if (!Array.isArray(values[input.name])) {
-                                    values[input.name] = [];
-                                }
+      // Lấy value từ form
+      if (isFormValid) {
+        if (typeof options.onSubmit === 'function') {
+          const enableInputs = formEl.querySelectorAll('[name]')
+          const formValues = Array.from(enableInputs).reduce(
+            (values, input) => {
+              switch (input.type) {
+                case 'radio':
+                  values[input.name] = formEl.querySelector(
+                    'input[name="' + input.name + '"]:checked',
+                  ).value
+                  break
+                case 'checkbox':
+                  if (!Array.isArray(values[input.name])) {
+                    values[input.name] = []
+                  }
 
-                                if (input.matches(":checked")) {
-                                    values[input.name].push(input.value);
-                                }
+                  if (input.matches(':checked')) {
+                    values[input.name].push(input.value)
+                  }
 
-                                if (values[input.name].length === 0) {
-                                    values[input.name] = "";
-                                }
-                                break;
-                            case "file":
-                                values[input.name] = input.files;
-                                break;
-                            default:
-                                values[input.name] = input.value;
-                        }
-                        return values;
-                    }, {});
-                    options.onSubmit(formValues);
-                } else {
-                    formEl.submit();
-                }
-            }
-        };
-
-        options.rules.forEach((rule) => {
-            // Lưu lại rule cho mỗi input
-            if (!Array.isArray(selectorRules[rule.selector])) {
-                selectorRules[rule.selector] = [];
-            }
-            selectorRules[rule.selector].push(rule.test);
-
-            const inputEls = formEl.querySelectorAll(rule.selector);
-            Array.from(inputEls).forEach((inputEl) => {
-                const formGroupEl = getParent(inputEl, options.formGroupSelector);
-                const errorEl = formGroupEl.querySelector(options.errorSelector);
-                if (inputEl) {
-                    inputEl.onblur = () => {
-                        validate(rule, inputEl);
-                    };
-
-                    inputEl.onchange = () => {
-                        validate(rule, inputEl);
-                    };
-
-                    inputEl.oninput = () => {
-                        errorEl.innerText = "";
-                        formGroupEl.classList.remove("invalid");
-                    };
-                }
-            });
-        });
-    }
-
-    function validate(rule, inputEl) {
-        const formGroupEl = getParent(inputEl, options.formGroupSelector);
-        const errorEl = formGroupEl.querySelector(options.errorSelector);
-        const rules = selectorRules[rule.selector];
-        let errorMessage;
-
-        // Lặp qua rules và kiểm tra
-        for (let i = 0; i < rules.length; i++) {
-            switch (inputEl.type) {
-                case "radio":
-                    errorMessage = rules[i](formGroupEl.querySelector(rule.selector + ":checked"));
-                    break;
-                case "checkbox":
-                    errorMessage = rules[i](formGroupEl.querySelectorAll(rule.selector + ":checked"));
-                    break;
+                  if (values[input.name].length === 0) {
+                    values[input.name] = ''
+                  }
+                  break
+                case 'file':
+                  values[input.name] = input.files
+                  break
                 default:
-                    errorMessage = rules[i](inputEl.value);
-            }
-            if (errorMessage) break;
-        }
-
-        if (errorMessage) {
-            errorEl.innerText = errorMessage;
-            formGroupEl.classList.add("invalid");
+                  values[input.name] = input.value
+              }
+              return values
+            },
+            {},
+          )
+          options.onSubmit(formValues)
         } else {
-            errorEl.innerText = "";
-            formGroupEl.classList.remove("invalid");
+          formEl.submit()
         }
-
-        return !!errorMessage;
+      }
     }
 
-    function getParent(el, selector) {
-        while (el.parentElement) {
-            if (el.parentElement.matches(selector)) {
-                return el.parentElement;
-            }
-            el = el.parentElement;
+    options.rules.forEach((rule) => {
+      // Lưu lại rule cho mỗi input
+      if (!Array.isArray(selectorRules[rule.selector])) {
+        selectorRules[rule.selector] = []
+      }
+      selectorRules[rule.selector].push(rule.test)
+
+      const inputEls = formEl.querySelectorAll(rule.selector)
+      Array.from(inputEls).forEach((inputEl) => {
+        const formGroupEl = getParent(inputEl, options.formGroupSelector)
+        const errorEl = formGroupEl.querySelector(options.errorSelector)
+        if (inputEl) {
+          inputEl.onblur = () => {
+            validate(rule, inputEl)
+          }
+
+          inputEl.onchange = () => {
+            validate(rule, inputEl)
+          }
+
+          inputEl.oninput = () => {
+            errorEl.innerText = ''
+            formGroupEl.classList.remove('invalid')
+          }
         }
+      })
+    })
+  }
+
+  function validate(rule, inputEl) {
+    const formGroupEl = getParent(inputEl, options.formGroupSelector)
+    const errorEl = formGroupEl.querySelector(options.errorSelector)
+    const rules = selectorRules[rule.selector]
+    let errorMessage
+
+    // Lặp qua rules và kiểm tra
+    for (let i = 0; i < rules.length; i++) {
+      switch (inputEl.type) {
+        case 'radio':
+          errorMessage = rules[i](
+            formGroupEl.querySelector(rule.selector + ':checked'),
+          )
+          break
+        case 'checkbox':
+          errorMessage = rules[i](
+            formGroupEl.querySelectorAll(rule.selector + ':checked'),
+          )
+          break
+        default:
+          errorMessage = rules[i](inputEl.value)
+      }
+      if (errorMessage) break
     }
+
+    if (errorMessage) {
+      errorEl.innerText = errorMessage
+      formGroupEl.classList.add('invalid')
+    } else {
+      errorEl.innerText = ''
+      formGroupEl.classList.remove('invalid')
+    }
+
+    return !!errorMessage
+  }
+
+  function getParent(el, selector) {
+    while (el.parentElement) {
+      if (el.parentElement.matches(selector)) {
+        return el.parentElement
+      }
+      el = el.parentElement
+    }
+  }
 }
 
 // Rules
 validator.isRequired = (selector, message) => {
-    return {
-        selector,
-        test(value) {
-            return value.trim() ? undefined : message || "Vui lòng nhập trường này";
-        },
-    };
-};
+  return {
+    selector,
+    test(value) {
+      return value.trim() ? undefined : message || 'Vui lòng nhập trường này'
+    },
+  }
+}
 
 validator.isEmail = (selector, message) => {
-    return {
-        selector,
-        test(value) {
-            if (!value) {
-                return undefined;
-            }
-            const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-            return regex.test(value) ? undefined : message || "Email không hợp lệ";
-        },
-    };
-};
+  return {
+    selector,
+    test(value) {
+      if (!value) {
+        return undefined
+      }
+      const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+      return regex.test(value) ? undefined : message || 'Email không hợp lệ'
+    },
+  }
+}
 
 validator.isPhoneNumber = (selector, message) => {
-    return {
-        selector,
-        test(value) {
-            if (!value) {
-                return undefined;
-            }
-            const regex = /^0\d{9}$/;
-            return regex.test(value) ? undefined : message || "Số điện thoại không hợp lệ";
-        },
-    };
-};
+  return {
+    selector,
+    test(value) {
+      if (!value) {
+        return undefined
+      }
+      const regex = /^0\d{9}$/
+      return regex.test(value)
+        ? undefined
+        : message || 'Số điện thoại không hợp lệ'
+    },
+  }
+}
 
 validator.minLength = (selector, min, message) => {
-    return {
-        selector,
-        test(value) {
-            return value.length >= min ? undefined : message || `Vui lòng nhập tối thiểu ${min} ký tự`;
-        },
-    };
-};
+  return {
+    selector,
+    test(value) {
+      return value.length >= min
+        ? undefined
+        : message || `Vui lòng nhập tối thiểu ${min} ký tự`
+    },
+  }
+}
 
 validator.maxLength = (selector, max, message) => {
-    return {
-        selector,
-        test(value) {
-            return value.length <= max ? undefined : message || `Vui lòng nhập tối đa ${max} ký tự`;
-        },
-    };
-};
+  return {
+    selector,
+    test(value) {
+      return value.length <= max
+        ? undefined
+        : message || `Vui lòng nhập tối đa ${max} ký tự`
+    },
+  }
+}
 
 validator.isConfirmed = (selector, getConfirmValue, message) => {
-    return {
-        selector,
-        test(value) {
-            return value === getConfirmValue() ? undefined : message || "Giá trị nhập vào không chính xác";
-        },
-    };
-};
+  return {
+    selector,
+    test(value) {
+      return value === getConfirmValue()
+        ? undefined
+        : message || 'Giá trị nhập vào không chính xác'
+    },
+  }
+}
 
 validator.isSelected = (selector, message) => {
-    return {
-        selector,
-        test(value) {
-            if (value instanceof NodeList) {
-                return value.length > 0 ? undefined : message || "Vui lòng chọn tùy chọn phù hợp";
-            }
-            return value ? undefined : message || "Vui lòng chọn tùy chọn phù hợp";
-        },
-    };
-};
+  return {
+    selector,
+    test(value) {
+      if (value instanceof NodeList) {
+        return value.length > 0
+          ? undefined
+          : message || 'Vui lòng chọn tùy chọn phù hợp'
+      }
+      return value ? undefined : message || 'Vui lòng chọn tùy chọn phù hợp'
+    },
+  }
+}
 
 validator.minChecked = (selector, min, message) => {
-    return {
-        selector,
-        test(value) {
-            return value.length >= min ? undefined : message || `Vui lòng chọn tối thiểu ${min} lựa chọn`;
-        },
-    };
-};
+  return {
+    selector,
+    test(value) {
+      return value.length >= min
+        ? undefined
+        : message || `Vui lòng chọn tối thiểu ${min} lựa chọn`
+    },
+  }
+}
 
 validator.maxChecked = (selector, max, message) => {
-    return {
-        selector,
-        test(value) {
-            return value.length <= max ? undefined : message || `Vui lòng chọn tối đa ${max} lựa chọn`;
-        },
-    };
-};
+  return {
+    selector,
+    test(value) {
+      return value.length <= max
+        ? undefined
+        : message || `Vui lòng chọn tối đa ${max} lựa chọn`
+    },
+  }
+}
